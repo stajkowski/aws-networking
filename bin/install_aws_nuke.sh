cf953e1577c19f70ee48195d05fd174df13374a3
Brian Stajkowski <stajkowski100@gmail.com>
2024-02-14 14:53:26 -0800

    Fix Nuke Runner

--- bin/install_aws_nuke.sh
+++ bin/install_aws_nuke.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Terraform AWS Nuke
-wget -c https://github.com/rebuy-de/aws-nuke/releases/download/v2.25.0/aws-nuke-v2.25.0-linux-amd64.tar.gz -O - | tar -xz -C $HOME/.local/bin
-mv $HOME/.local/bin/aws-nuke-v2.25.0-linux-amd64 $HOME/.local/bin/aws-nuke
+wget -c https://github.com/rebuy-de/aws-nuke/releases/download/v2.25.0/aws-nuke-v2.25.0-linux-amd64.tar.gz -O - | tar -xz -C $PWD
+mv $PWD/aws-nuke-v2.25.0-linux-amd64 $PWD/aws-nuke
--- bin/run_nuke.sh
+++ bin/run_nuke.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script will run live integration tests
-$HOME/.local/bin/aws-nuke -c ./config/nuke_config.yml --force --no-dry-run
+$PWD/aws-nuke -c ./config/nuke_config.yml --force --no-dry-run
